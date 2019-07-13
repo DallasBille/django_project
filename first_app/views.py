07e89cae2ed29d3ddb916457148f9581ddc10b03
github username <dallasbille@gmail.com>
2019-07-13 11:01:24 -0400

    created validation for second email input field, hiddenbotcatcher field

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from first_app.models import Topic,Webpage,AccessRecord, Users
+from first_app.models import Topic,Webpage,AccessRecord,Users
+from first_app import forms
 # This is where you have functions to handle requests and return responses.
 # Create your views here.
 # This is the controller section of Django
@@ -16,3 +17,22 @@ def users(request):
     user_list = Users.objects.order_by('first_name')
     user_dict = {'users' : user_list}
     return render(request, 'first_app/users.html',context=user_dict)
+
+def form_name(request):
+    form = forms.FormName()
+    # the above point to the class.
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        print(request.POST)
+        # request.POST give you a bunch of juicy stuff, like key values of form fields(name, email, text). As well as other jargon. THis is where you extract data.
+
+        if form.is_valid():
+            print("valid")
+            print("Name: "+ form.cleaned_data['name'])
+            print("Email: "+ form.cleaned_data['email'])
+            print("Text: "+ form.cleaned_data['text'])
+
+
+
+    return render(request,'first_app/form.html', {'form':form})
